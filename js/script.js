58ffca7121a60e665a966b78641ed94e243bd1a6
arianna-cosimini <arianna.cosimini@gmail.com>
2024-02-07 19:43:35 +0100

    esercizio finito

--- js/script.js
+++ js/script.js
@@ -1,23 +1,53 @@
 
 
+
 const myButton = document.querySelector("#btn-play");
 myButton.addEventListener("click", function () {
+    
     const gridElement = document.querySelector("#grid");
+    gridElement.innerHTML = ''; 
     const randomNumbersArray = getRandomNumbersArray();
-    //  creo elementi con classe square
-    for (let i = 0; i < 100; i++) {
-        const newElement = document.createElement("div");
-        newElement.classList.add("square");
-        newElement.innerText = randomNumbersArray[i];
-        // aggiungo classe active a square al click
-        newElement.addEventListener("click", function () {
-            console.log(this);
-            this.classList.toggle("active");
-        });
 
-        gridElement.append(newElement);
-    }
+
+    const userChoice = document.querySelector("select").value;
+
+    if(userChoice == 1){
+        for (let i = 0; i < 100; i++) {
+            newClass(gridElement, randomNumbersArray, i);
+        }
+    }else if (userChoice == 2){
+        for (let i = 0; i < 81; i++) {
+            newClass(gridElement, randomNumbersArray, i);
+            
+        }
+    }else{
+        for (let i = 0; i < 49; i++) {
+            newClass(gridElement, randomNumbersArray, i);
+        }}
+
 });
+
+function newClass (gridElement, randomNumbersArray, i){
+    const newElement = document.createElement("div");
+    newElement.classList.add("square");
+    newElement.innerText = randomNumbersArray[i];
+    // aggiungo classe active a square al click
+    const userChoice = document.querySelector("select").value;
+    // livello di difficoltà scelto dall'utente
+    if (userChoice == 2) {
+        newElement.classList.add("medium");
+    }else if( userChoice == 3){
+        newElement.classList.add("easy")
+    }
+
+
+    newElement.addEventListener("click", function () {
+        console.log(this);
+        this.classList.toggle("active");
+    });
+
+    gridElement.append(newElement);
+}
 
 // genero un numero casuale indicando come parametro un numero massimo 
 function generateRandomNumber(maxNumber) {
